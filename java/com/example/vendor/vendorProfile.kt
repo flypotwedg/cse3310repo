a6a3b7bf9f34eb656aad23313bc4812b3e327e8b
flypotwedg <52862940+flypotwedg@users.noreply.github.com>
2022-04-13 22:27:14 -0500

    Add files via upload

---
+++ java/com/example/vendor/vendorProfile.kt
@@ -0,0 +1,14 @@
+package com.example.vendor
+
+import android.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class vendorProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
